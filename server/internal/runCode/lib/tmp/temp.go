b65b565ff167377426905d699ecd789630472416
Gonrs <one642077@gmail.com>
2025-04-07 20:40:27 +0300

    small fix

---
+++ server/internal/runCode/lib/tmp/temp.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt" 
+ 	"math" 
+)
+
+func main() {
+	math.Abs(1)
+	var (
+		a int
+		b int
+	)
+	fmt.Scanln(&a, &b)
+	fmt.Println(Code(a, b))
+}
+
+// use can use fmt package
+func Code(a int, b int) int{
+
+}
